09f4aa4c2912c798f2aa61cd5c173f0cc60f9584
Osmar Gonzalez <osmarlenin.gonzalezescorche@externos-cl.cencosud.com>
2024-02-16 20:08:51 -0300

    Add ProductsApp stack and ProductsFetchFunction
    This commit introduces the ProductsApp stack and the ProductsFetchFunction along with their respective dependencies. The stack is defined in 'lib/productsApp-stack.ts' and the function in 'lambda/products/productsFetchFunction.ts'. Additionally, the necessary dependencies for these constructs have been added to the 'package.json' and 'package-lock.json' files.

---
+++ lambda/products/productsFetchFunction.ts
@@ -0,0 +1,31 @@
+import {APIGatewayProxyEvent, APIGatewayProxyResult, Context} from "aws-lambda";
+
+export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
+
+    const method = event.httpMethod
+
+    let lambdaRequestId = context.awsRequestId;
+    let apiRequestId = event.requestContext.requestId;
+
+    console.log(`API Gateway RequestId: ${apiRequestId} - Lambda RequestId: ${lambdaRequestId}`)
+
+    if(event.resource == "/products") {
+        if(method === 'GET') {
+           console.log('GET')
+
+           return {
+               statusCode: 200,
+               body: JSON.stringify({
+                   message: "GET Products - OK"
+               })
+           }
+        }
+    }
+
+    return {
+        statusCode: 400,
+        body: JSON.stringify({
+            message: "Bad Request"
+        })
+    }
+}
---
+++ lib/productsApp-stack.ts
@@ -0,0 +1,24 @@
+import * as lambda from "aws-cdk-lib/aws-lambda"
+import * as lambdaNodeJS from "aws-cdk-lib/aws-lambda-nodejs"
+import * as cdk from "aws-cdk-lib"
+import { Construct } from "constructs"
+
+export class ProductsAppStack extends cdk.Stack {
+    readonly productsFetchHandler: lambdaNodeJS.NodejsFunction
+    constructor(scope: Construct, id: string, props?: cdk.StackProps ) {
+        super(scope, id, props);
+
+        this.productsFetchHandler = new lambdaNodeJS.NodejsFunction(this, "ProductsFetchFunction", {
+            runtime: lambda.Runtime.NODEJS_20_X,
+            memorySize: 512,
+            functionName: "ProductsFetchFunction",
+            entry: "lambda/products/productsFetchFunction.ts",
+            handler: "handler",
+            timeout: cdk.Duration.seconds(5),
+            bundling: {
+                minify: true,
+                sourceMap: false
+            },
+        })
+    }
+}
